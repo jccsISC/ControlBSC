f0f7171090d3d89c0d4cd4acbf4e183af1ff179c
jccsISC <silva.jccs18@gmail.com>
2020-03-03 01:12:50 -0600

    agregando navigationDrawer

--- app/src/main/java/com/jccsisc/controlbsc/LoginActivity.java
+++ app/src/main/java/com/jccsisc/controlbsc/LoginActivity.java
@@ -2,6 +2,7 @@ package com.jccsisc.controlbsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +16,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+//                finish();
+                break;
+        }
     }
 }
