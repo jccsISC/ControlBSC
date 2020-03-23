a852e55f288cedd1d3392d2d0ebb9200493deff0
jccsISC <silva.jccs18@gmail.com>
2020-03-23 14:05:29 -0600

    modificar producto con un alrt

--- app/src/main/java/com/jccsisc/controlbsc/ui/productos/ProductosFragment.java
+++ app/src/main/java/com/jccsisc/controlbsc/ui/productos/ProductosFragment.java
@@ -1,18 +1,25 @@
 package com.jccsisc.controlbsc.ui.productos;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,6 +27,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.jccsisc.controlbsc.R;
+import com.jccsisc.controlbsc.activities.RegistrarE_S_C_Activity;
 import com.jccsisc.controlbsc.adapters.ProductosAdapter;
 import com.jccsisc.controlbsc.model.Detalle;
 import com.jccsisc.controlbsc.model.Movimiento;
@@ -37,7 +45,7 @@ public class ProductosFragment extends Fragment {
     private ArrayList<Producto> productoArrayList;
     private ProductosAdapter productosAdapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_productos, container, false);
         rvProductos = v.findViewById(R.id.recyclerProductos);
@@ -99,6 +107,53 @@ public class ProductosFragment extends Fragment {
             }
         });
 
+        productosAdapter.setOnClickListener(new ProductosAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(final int pos) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                alertDialog.setTitle("MODIFICAR NOMBRE PRODUCTO");
+                alertDialog.setMessage(String.valueOf(productoArrayList.get(pos).getName()));
+
+                final EditText input = new EditText(getActivity());
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                alertDialog.setView(input);
+
+                alertDialog.setPositiveButton("Guardar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(final DialogInterface dialog, int which) {
+                                String idKey = productoArrayList.get(pos).getIdKey();
+
+                                myRef.child("DB_Productos").child(idKey).child("name").setValue(input.getText().toString().toUpperCase()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        mtoast("Modificado correctamente");
+                                        dialog.dismiss();
+                                    }
+                                });
+                            }
+                        });
+
+                alertDialog.setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog.show();
+            }
+        });
+
         return v;
+    }
+
+
+    public void mtoast(String msj) {
+        Toast toast = Toast.makeText(getContext(), msj, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
